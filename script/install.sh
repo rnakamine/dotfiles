8bb6f161844d56695b3c212e395206c532acbeb6
Ryo Nakamine <rnakamine8080@gmail.com>
2021-01-05 20:59:42 +0900

    add gitconfig

--- script/install.sh
+++ script/install.sh
@@ -3,17 +3,18 @@
 DIR=$(cd $(dirname $0)/.. && pwd)
 
 CONFIGS=(
-    zshrc
-    vimrc
-    tmux.conf
+  zshrc
+  vimrc
+  tmux.conf
+  gitconfig
 )
 
 for CONFIG in ${CONFIGS[@]}
 do
-    if [ ! -f ${HOME}/.${CONFIG} ]; then
-        echo "[INFO] Place ${HOME}/.${CONFIG}"
-        ln -s ${DIR}/${CONFIG} ${HOME}/.${CONFIG}
-    fi
+  if [ ! -f ${HOME}/.${CONFIG} ]; then
+    echo "[INFO] Place ${HOME}/.${CONFIG}"
+    ln -s ${DIR}/${CONFIG} ${HOME}/.${CONFIG}
+  fi
 done
 
 echo "[INFO] Done."
