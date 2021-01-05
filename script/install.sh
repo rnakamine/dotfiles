5b2a9df633497fa8bedb0036e6f643de7e566d74
Ryo Nakamine <rnakamine8080@gmail.com>
2021-01-05 20:48:01 +0900

    add install script

---
+++ script/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+DIR=$(cd $(dirname $0)/.. && pwd)
+
+CONFIGS=(
+    zshrc
+    vimrc
+    tmux.conf
+)
+
+for CONFIG in ${CONFIGS[@]}
+do
+    if [ ! -f ${HOME}/.${CONFIG} ]; then
+        echo "[INFO] Place ${HOME}/.${CONFIG}"
+        ln -s ${DIR}/${CONFIG} ${HOME}/.${CONFIG}
+    fi
+done
+
+echo "[INFO] Done."
